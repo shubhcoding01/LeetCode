5cb8c0fa3a9d94a9d8330d3e5655856a91a3886a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2026-01-03 10:00:04 +0530

    Updated

--- 3_Kids With the Greatest Number of Candies.java
+++ 3_Kids With the Greatest Number of Candies.java
@@ -56,3 +56,24 @@ class Solution {
         return result;
     }
 }
+
+
+// class Solution {
+//     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//         int max = 0;
+//         for (int i = 0; i < candies.length; i++) {
+//             if (candies[i] > max) {
+//                 max = candies[i];
+//             }
+//         }
+//         List <Boolean> result = new ArrayList<>(candies.length);
+//         for(int i = 0; i < candies.length; i++) {
+//             if(candies[i] + extraCandies >= max) {
+//                 result.add(true);
+//             } else {
+//                 result.add(false);
+//             }
+//         }
+//         return result;
+//     }
+// }
