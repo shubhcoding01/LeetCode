5d9ed03a4aa56e6f3d79da461d9382e633780a00
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2026-01-05 15:22:05 +0530

    Updated

--- 5_Reverse_vowels_of_String.java
+++ 5_Reverse_vowels_of_String.java
@@ -1,3 +1,33 @@
+// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+// Example 1:
+
+// Input: s = "IceCreAm"
+
+// Output: "AceCreIm"
+
+// Explanation:
+
+// The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+// Example 2:
+
+// Input: s = "leetcode"
+
+// Output: "leotcede"
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 3 * 105
+// s consist of printable ASCII characters.
+
+
 class Solution {
     public String reverseVowels(String s) {
         char[] arr = s.toCharArray();
@@ -24,3 +54,59 @@ class Solution {
         return "aeiouAEIOU".indexOf(c) != -1;
     }
 }
+
+// class Solution {
+//     static {
+//         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//             try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+//                 fw.write("0");
+//             } catch (Exception e) {
+//             }
+//         }));
+//     }
+//     public String reverseVowels(String str) 
+//     {
+//         Set<Character> set = new HashSet<>();
+//         set.add('a');
+//         set.add('e');
+//         set.add('i');
+//         set.add('o');
+//         set.add('u');
+//         set.add('A');
+//         set.add('E');
+//         set.add('I');
+//         set.add('O');
+//         set.add('U');
+        
+//         StringBuilder s = new StringBuilder(str);
+//         int left=0,right=str.length()-1;
+//         while(left<right)
+//         {
+//             if(set.contains(s.charAt(left)) && set.contains(s.charAt(right)))
+//             {
+//                 char temp=s.charAt(left);
+//                 s.setCharAt(left, s.charAt(right));
+//                 s.setCharAt(right, temp);
+//                 left++;
+//                 right--;
+//             }
+
+//             else if(set.contains(s.charAt(left)) && !set.contains(s.charAt(right)))
+//             {
+//                 right--;
+//             }
+
+//             else if(!set.contains(s.charAt(left)) && set.contains(s.charAt(right)))
+//             {
+//                 left++;
+//             }
+
+//             else
+//             {
+//                 left++;
+//                 right--;
+//             }
+//         }
+//         return s.toString();   
+//     }
+// }
