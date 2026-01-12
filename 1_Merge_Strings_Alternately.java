3f543ce66ad6d3a4e45d536a2fe09981f96351f3
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2026-01-12 22:22:35 +0530

    Updated

--- 1_Merge_Strings_Alternately.java
+++ 1_Merge_Strings_Alternately.java
@@ -59,6 +59,30 @@ class Solution {
     }
 }
 
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        int j = 0;
+
+        while(i < word1.length() && j < word2.length()){
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        if(i < word1.length()){
+            result.append(word1.substring(i));
+        }
+        if(j < word2.length()){
+            result.append(word2.substring(j));
+        }
+
+        return result.toString();
+
+    }
+}
+
 // [Other 0ms Solution ]
 // class Solution {
 //     public String mergeAlternately(String word1, String word2) {
