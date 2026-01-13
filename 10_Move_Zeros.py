2778bfff4847cba263b0968fe88ec64cad7a9891
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2026-01-13 20:43:05 +0530

    Updated

--- 10_Move_Zeros.py
+++ 10_Move_Zeros.py
@@ -14,3 +14,24 @@ class Solution:
         for i in range(pos, len(nums)):
             nums[i] = 0
         
+# 🔍 Dry Run (Important)
+# Input
+# nums = [0,1,0,3,12]
+
+# Process
+# pos = 0
+# i=0 → 0 → skip
+# i=1 → 1 → nums[0]=1 → pos=1
+# i=2 → 0 → skip
+# i=3 → 3 → nums[1]=3 → pos=2
+# i=4 → 12 → nums[2]=12 → pos=3
+
+
+# Array now:
+
+# [1,3,12,?,?]
+
+
+# Fill zeroes:
+
+# [1,3,12,0,0]
